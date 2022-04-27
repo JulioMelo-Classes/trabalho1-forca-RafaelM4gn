66b68b46acec8655294f4683168409f4e2e98927
RafaelM4gn <rfl.magno02@gmail.com>
2022-04-26 23:51:57 -0300

    update3

--- src/Forca.cpp
+++ src/Forca.cpp
@@ -8,95 +8,90 @@
 #include "Forca.hpp"
 using namespace std;
 
-/**
-         * Cria a classe Forca
-         * O construtor pode ou não validar os arquivos de entrada, no entanto, mesmo com 
-         * os arquivos inválidos o objeto deve ser construído. Para o caso de arquivos de palavras
-         * ou scores inválidos, use o método eh_valido.
-         * @param palavras o arquivo que contém as palavras
-         * @param scores o nome do arquivo que contém os scores
-         * @see eh_valido
-         */
 Forca::Forca(string palavras, string scores)
 {
-  m_arquivo_palavras = palavras;
-  m_arquivo_scores = scores;
+    m_arquivo_palavras = palavras;
+    m_arquivo_scores = scores;
 };
 
-void Forca::set_dificuldade(Dificuldade d){
-  
+void Forca::set_dificuldade(Dificuldade d)
+{
 }
 pair<bool, string> Forca::eh_valido()
 {
-  fstream ValidaScores;
-  fstream ValidaScores2;
-  fstream ValidaPalavras;
-  string frequencia;
-  string palavra;
-  ostringstream oss;
+    fstream ValidaScores;
+    fstream ValidaScores2;
+    fstream ValidaPalavras;
+    string frequencia;
+    string palavra;
+    ostringstream oss;
 
-  int line = 0;
-  ValidaPalavras.open(m_arquivo_palavras, ios::in);
-  if (ValidaPalavras.fail()) {
-      oss << "Arquivo " << m_arquivo_palavras << " nao encontrado";
-      return make_pair(false, oss.str());
-  }
-  
-  while(!ValidaPalavras.eof()){
-    ValidaPalavras  >> frequencia >> palavra ;
-    line++;
-    if (palavra.size() <= 4)
+    int line = 0;
+    ValidaPalavras.open(m_arquivo_palavras, ios::in);
+    if (ValidaPalavras.fail())
     {
-        oss << "Uma das palavras possui frequência menor que 4. Palavra: " << palavra << " Linha: "<< line;
+        oss << "Arquivo " << m_arquivo_palavras << " nao encontrado";
         return make_pair(false, oss.str());
     }
-    if (frequencia.find_first_not_of("0123456789") != string::npos)
+
+    while (!ValidaPalavras.eof())
     {
-      oss << "Frequência de uma das palavras Inválida. Palavra: "  << palavra << " Linha: "<< line;
-      return make_pair(false, oss.str());
+        ValidaPalavras >> frequencia >> palavra;
+        line++;
+        if (palavra.size() <= 4)
+        {
+            oss << "Uma das palavras possui frequência menor que 4. Palavra: " << palavra << " Linha: " << line;
+            return make_pair(false, oss.str());
+        }
+        if (frequencia.find_first_not_of("0123456789") != string::npos)
+        {
+            oss << "Frequência de uma das palavras Inválida. Palavra: " << palavra << " Linha: " << line;
+            return make_pair(false, oss.str());
+        }
+        if (palavra.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-") != string::npos)
+        {
+            oss << "Palavras com caracteres inválidos. Palavra: " << palavra << " Linha: " << line;
+            return make_pair(false, oss.str());
+        }
     }
-    if (palavra.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-") != string::npos)
+    ValidaScores.open(m_arquivo_scores, ios::in);
+    if (ValidaScores.fail())
     {
-      oss << "Palavras com caracteres inválidos. Palavra: "  << palavra << " Linha: "<< line;
-      return make_pair(false, oss.str());
+        oss << "Arquivo " << m_arquivo_scores << " nao encontrado";
+        return make_pair(false, oss.str());
     }
-  }
-  ValidaScores.open(m_arquivo_scores, ios::in);
-  if (ValidaScores.fail()) {
-      oss << "Arquivo " << m_arquivo_scores << " nao encontrado";
-      return make_pair(false, oss.str());
-  }
-  string dificulty;
-  string nome;
-  string pontuacao;
-  string array;
-  while (!ValidaScores.eof())
-  {
-    getline(ValidaScores,dificulty,';');  
-    getline(ValidaScores,nome,';');
-    getline(ValidaScores,array,';');
-    getline(ValidaScores,pontuacao),'\n';
-    if (dificulty.empty()||nome.empty()||pontuacao.empty())
+    string dificulty, nome, pontuacao, array;
+    line = 0;
+    while (!ValidaScores.eof())
     {
-      return  make_pair(false,"Um dos campos está vazio");
+        line++;
+        getline(ValidaScores, dificulty, ';');
+        getline(ValidaScores, nome, ';');
+        getline(ValidaScores, array, ';');
+        getline(ValidaScores, pontuacao), '\n';
+        if (dificulty.empty() || nome.empty() || pontuacao.empty())
+        {
+            oss << "Um dos campos está vazio. Linha: " << line;
+            return make_pair(false, oss.str());
+        }
     }
-  }
-  ValidaScores2.open(m_arquivo_scores, ios::in);
-  while (!ValidaScores2.eof())
-  { 
-    getline(ValidaScores2,array);
-    int quantidade = 0;
-    for(int i =0;i<array.size();i++)
+    ValidaScores2.open(m_arquivo_scores, ios::in);
+    line = 0;
+    while (!ValidaScores2.eof())
     {
-      if(array[i]==';')
-      {
-        quantidade +=1;
-      }
+        line++;
+        int quantidade = 0;
+        getline(ValidaScores2, array);
+        for (int i = 0; i < array.size(); i++)
+        {
+            if (array[i] == ';')
+                quantidade++;
+        }
+        if (quantidade >= 4)
+        {
+            oss << "Quantidade de ; maior que o esperado. Linha: " << line;
+            return make_pair(false, oss.str());
+        }
     }
-    if(quantidade>=4)
-    {
-      return make_pair(false,"Quantidade de ; maior que o esperado");
-    }
-  }
-  return make_pair(true, "");
+    return make_pair(true, "");
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,48 +3,53 @@
 #include "Forca.hpp"
 
 using namespace std;
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     fstream arquivoPonto;
-    
+
     Forca forca = Forca(argv[1], argv[2]);
     cout << ">>> Lendo arquivos de palavras"
-       << " [" << argv[1] << "] "
-       << "e scores"
-       << " [" << argv[2] << "] ,"
-       << "por favor aguarde.." << endl; 
-    //forca.carregar_arquivos();
+         << " [" << argv[1] << "] "
+         << "e scores"
+         << " [" << argv[2] << "] ,"
+         << "por favor aguarde.." << endl;
+    // forca.carregar_arquivos();
     auto valid = forca.eh_valido();
-    if(!valid.first){ //sai do programa ao encontrar um erro
-        cout<<"Erro "<<valid.second<<endl;
+    if (!valid.first)
+    { // sai do programa ao encontrar um erro
+        cout << "Erro " << valid.second << endl;
         exit(-1);
     }
-    cout<<"--------------------------------------------------------------------"<<endl;
-    cout<<">>> Arquivos OK!"<<endl;
-    
-    while(true){
+    cout << "--------------------------------------------------------------------" << endl;
+    cout << ">>> Arquivos OK!" << endl;
+
+    while (true)
+    {
         /*imprime o menu principal e a escolha do usuário*/
         int pontos = 0;
         int escolha;
         int escolhaD;
-        cout << "--------------------------------------------------------------------" << endl;
-        cout << "Bem vindo ao Jogo Forca! Por favor escolha uma das opções" << endl;
-        cout << "1 - Iniciar Jogo" << endl;
-        cout << "2 - Ver scores anteriores" << endl;
-        cout << "3 - Sair do Jogo" << endl;
-        cout << "sua escolha:";
+        cout << "--------------------------------------------------------------------" << endl
+             << "Bem vindo ao Jogo Forca! Por favor escolha uma das opções" << endl
+             << "1 - Iniciar Jogo" << endl
+             << "2 - Ver scores anteriores" << endl
+             << "3 - Sair do Jogo" << endl
+             << "sua escolha:";
         cin >> escolha;
         if (escolha == 1)
         {
-        cout << "Vamos iniciar o jogo! Por favor escolha o nível de dificuldade" << endl
-            << "1 - Fácil" << endl
-            << "2 - Médio" << endl
-            << "3 - Difícil" << endl;
-        cout << "Sua escolha de dificuldade: ";
-        cin >> escolhaD;
-        if (escolhaD == 1) forca.set_dificuldade(forca.FACIL);
-        else if (escolhaD == 2) forca.set_dificuldade(forca.MEDIO);
-        else if (escolhaD == 3) forca.set_dificuldade(forca.DIFICIL);
-
+            cout << "Vamos iniciar o jogo! Por favor escolha o nível de dificuldade" << endl
+                 << "1 - Fácil" << endl
+                 << "2 - Médio" << endl
+                 << "3 - Difícil" << endl
+                 << "Sua escolha de dificuldade: ";
+            cin >> escolhaD;
+            if (escolhaD == 1)
+                forca.set_dificuldade(forca.FACIL);
+            else if (escolhaD == 2)
+                forca.set_dificuldade(forca.MEDIO);
+            else if (escolhaD == 3)
+                forca.set_dificuldade(forca.DIFICIL);
         }
         else if (escolha == 2)
         {
@@ -55,21 +60,21 @@ int main(int argc, char *argv[]){
                 getline(arquivoPonto, pontos);
                 cout << pontos << endl;
             }
-            
-            cout<<"Deseja Jogar?"<<endl
-                <<"1 - Sim"<<endl
-                <<"2 - Não"<<endl;
-                int decisao;
+
+            cout << "Voltar ao menu?" << endl
+                 << "1 - Sim" << endl
+                 << "2 - Não" << endl;
+            int decisao;
             cout << "Escolha: ";
             cin >> decisao;
             if (decisao == 2)
             {
                 break;
             }
-
-        } else if (escolha = 3){
-        exit(3);
-
+        }
+        else if (escolha = 3)
+        {
+            exit(3);
         }
         // if (escolha == 1)
         // {
@@ -79,36 +84,36 @@ int main(int argc, char *argv[]){
         //         << "3 - Difícil" << endl;
         //     cout << "Sua escolha de dificuldade: ";
         //     cin >> escolhaD;
-            /*Seleciona dificuldade*/
-            Forca::Dificuldade d;
-            //forca.set_dificuldade(d);
-//            while(true){ //
-//                string p = forca.proxima_palavra();
-                /*exibe interface do jogo*/
-//                while (!forca.rodada_terminada()){ //loop da rodada
-                    /*ler palpite*/
-//                    string palpite;
-//                    auto result = forca.palpite(palpite);
-                    /*testa palpite e atualiza a interface dependendo do resultado*/
-//                }
-//                if(true){ /*acertou a palavra inteira*/
-                    /*imprime interface de continuar / parar*/
-//                    if (/*parar*/)
-//                        break;
-//                    else
-//                        forca.reset_rodada();
-//                }
-//                else{ /*perdeu*/
-                    /*imprime gameover e a palavra que estava sendo jogada*/
-//                    break;
-//                }
-//            }
-            /*ler informações do jogador para o score e gravar no arquivo*/
-//        }
-//        else if(/*mostrar score*/)
-            /*mostrar score*/
-//        else //qualquer outro número sai do jogo
-//            break
+        /*Seleciona dificuldade*/
+        Forca::Dificuldade d;
+        // forca.set_dificuldade(d);
+        //            while(true){ //
+        //                string p = forca.proxima_palavra();
+        /*exibe interface do jogo*/
+        //                while (!forca.rodada_terminada()){ //loop da rodada
+        /*ler palpite*/
+        //                    string palpite;
+        //                    auto result = forca.palpite(palpite);
+        /*testa palpite e atualiza a interface dependendo do resultado*/
+        //                }
+        //                if(true){ /*acertou a palavra inteira*/
+        /*imprime interface de continuar / parar*/
+        //                    if (/*parar*/)
+        //                        break;
+        //                    else
+        //                        forca.reset_rodada();
+        //                }
+        //                else{ /*perdeu*/
+        /*imprime gameover e a palavra que estava sendo jogada*/
+        //                    break;
+        //                }
+        //            }
+        /*ler informações do jogador para o score e gravar no arquivo*/
+        //        }
+        //        else if(/*mostrar score*/)
+        /*mostrar score*/
+        //        else //qualquer outro número sai do jogo
+        //            break
     }
     return 0;
 }
